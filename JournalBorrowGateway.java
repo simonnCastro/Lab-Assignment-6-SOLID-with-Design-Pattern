cd4fe277e16dbc04ccd9f399be438426d69318ee
simonnCastro <61060425+simonnCastro@users.noreply.github.com>
2025-04-13 13:17:09 +0000

    added JournalBorrowGateway.java modified:   BookBorrowGateway.java modified:   TestOrder.java

--- JournalBorrowGateway.java
+++ JournalBorrowGateway.java
@@ -1,14 +1,14 @@
-public class BookBorrowGateway implements BorrowProcessor{
+public class JournalBorrowGateway implements BorrowProcessor{
     @Override
     public boolean authorizeBorrow(String title, StudentInfo studentInfo) {
         // Example implementation: Check if the student and title are valid
         if (studentInfo != null 
             && studentInfo.getStudentName() != null && !studentInfo.getStudentName().isEmpty() 
             && title != null && !title.isEmpty()){
-            System.out.println("Book Borrow authorized for student: " + studentInfo.getStudentName() + " and book: " + title);
+            System.out.println("Journal Borrow authorized for student: " + studentInfo.getStudentName() + " and book: " + title);
             return true;
         } else{
-            System.out.println("Book Borrow not authorized. Invalid student or book title.");
+            System.out.println("Journal Borrow not authorized. Invalid student or book title.");
             return false;
         }
 
--- TestOrder.java
+++ TestOrder.java
@@ -1,18 +1,31 @@
 public class TestOrder{
     public static void main(String[] args){
+        StudentInfo myStudentInfo1 = new StudentInfo();
+        myStudentInfo1.setStudentName("John Doe");
+        StudentInfo myStudentInfo2 = new StudentInfo();
+        myStudentInfo2.setStudentName("Jane Doe");
 
-        Order myOrder = new Order();
-        StudentInfo myStudentInfo = new StudentInfo();
-        myStudentInfo.setStudentName("John Doe");
-        
-        myOrder.setTitle("Harry Potter");
-        myOrder.setStudentInfo(myStudentInfo);
+        Order myOrder1 = new Order();
+        myOrder1.setTitle("Book1");
+        myOrder1.setStudentInfo(myStudentInfo1);
 
-        BorrowProcessor borrowProcessor = new BookBorrowGateway();
-
-        OrderProcessor orderProcessor = new OrderProcessor(borrowProcessor);
-
-        orderProcessor.processOrder(myOrder);
+        Order myOrder2 = new Order();
     
+        myOrder2.setTitle("Journal1");
+        myOrder2.setStudentInfo(myStudentInfo1);
+
+        Order myOrder3 = new Order();
+        myOrder3.setTitle("Book2");
+        myOrder3.setStudentInfo(myStudentInfo2);
+
+        BorrowProcessor bookBorrowProcessor = new BookBorrowGateway();
+        BorrowProcessor journalBorrowProcessor = new JournalBorrowGateway();
+
+        OrderProcessor bookOrderProcessor = new OrderProcessor(bookBorrowProcessor);
+        OrderProcessor journalOrderProcessor = new OrderProcessor(journalBorrowProcessor);
+
+        bookOrderProcessor.processOrder(myOrder1);
+        journalOrderProcessor.processOrder(myOrder2);
+        bookOrderProcessor.processOrder(myOrder3);
     }
 }
